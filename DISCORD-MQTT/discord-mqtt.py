e3041767721f4b927a0c3713640e6ce5a6b32c49
Estylos <esteban@estylos.fr>
2022-08-30 15:14:56 +0200

    Add logger

--- DISCORD-MQTT/discord-mqtt.py
+++ DISCORD-MQTT/discord-mqtt.py
@@ -1,7 +1,9 @@
 # Copyright (c) 2022 Esteban Cadic
 # This code is licensed under MIT license (see LICENSE file for details)
 
+# Dependencies : paho-mqtt ; discord.py
 import configparser
+import logging
 import paho.mqtt.client as mqtt
 import urllib.parse
 import discord
@@ -10,13 +12,17 @@ from discord.ext import commands
 # Config
 config = configparser.ConfigParser()
 config.read('config.ini')
+logging.basicConfig(filename='log.txt', filemode='w', format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s', level=logging.INFO)
+logger = logging.getLogger('discord-mqtt')
 
 
 # MQTT
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
         print("Connected to MQTT Broker!")
+        logger.info("Connected to MQTT Broker!")
     else:
+        logger.error("Failed to connect, return code {}".format(rc))
         raise Exception("Failed to connect, return code {}".format(rc))
 
 
@@ -39,13 +45,16 @@ async def write(ctx, *, arg):
         arg.encode('cp1252')
     except UnicodeEncodeError:
         await ctx.reply("Y'a des caractères que je ne peux pas afficher dans ton message...")
+        logger.error("Non-Windows-1252 character: {}".format(arg))
         raise
 
     if len(arg) > 40:
         await ctx.reply("Ton message est trop long :exploding_head:")
+        logger.error("Message too long: {}".format(arg))
         raise Exception("Message too long: {}".format(arg))
 
     print("Publication of the following message: {}".format(arg))
+    logger.info("Publication of the following message: {}".format(arg))
     client.publish(config['mqtt']['channel'],
                    urllib.parse.quote(arg, encoding='cp1252'))
     await ctx.message.add_reaction('\u2705')
